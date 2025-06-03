a6aa7a219804f91ae90c7c476f5bf0409e13c9e7
jaanus <zaar2213@gmail.com>
2025-06-03 20:05:05 +0300

    CP-32 complete

--- src/main/java/ee/bcs/carportal/persistence/Car.java
+++ src/main/java/ee/bcs/carportal/persistence/Car.java
@@ -3,6 +3,7 @@ package ee.bcs.carportal.persistence;
 import java.util.Objects;
 
 public class Car {
+    private int id;
     private String carModel;
     private String manufacturer;
     private int modelYear;
@@ -23,7 +24,25 @@ public class Car {
         this.price = price;
     }
 
-    // Getters and Setters
+    public Car(int id, String carModel, String manufacturer, int modelYear, FuelType fuelType, double emissions,
+            int price) {
+        this.id = id;
+        this.carModel = carModel;
+        this.manufacturer = manufacturer;
+        this.modelYear = modelYear;
+        this.fuelType = fuelType;
+        this.emissions = emissions;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getCarModel() {
         return carModel;
     }
@@ -73,22 +92,58 @@ public class Car {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return modelYear == car.modelYear && Double.compare(car.emissions, emissions) == 0 && price == car.price && carModel.equals(car.carModel) && manufacturer.equals(car.manufacturer) && fuelType == car.fuelType;
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((carModel == null) ? 0 : carModel.hashCode());
+        result = prime * result + ((manufacturer == null) ? 0 : manufacturer.hashCode());
+        result = prime * result + modelYear;
+        result = prime * result + ((fuelType == null) ? 0 : fuelType.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(emissions);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + price;
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(carModel, manufacturer, modelYear, fuelType, emissions, price);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        if (id != other.id)
+            return false;
+        if (carModel == null) {
+            if (other.carModel != null)
+                return false;
+        } else if (!carModel.equals(other.carModel))
+            return false;
+        if (manufacturer == null) {
+            if (other.manufacturer != null)
+                return false;
+        } else if (!manufacturer.equals(other.manufacturer))
+            return false;
+        if (modelYear != other.modelYear)
+            return false;
+        if (fuelType != other.fuelType)
+            return false;
+        if (Double.doubleToLongBits(emissions) != Double.doubleToLongBits(other.emissions))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
     }
 
     // toString() method
     @Override
     public String toString() {
-        return "Make: " + manufacturer + "\n" +
+        return "Id: " + id + "\n" +
+                "Make: " + manufacturer + "\n" +
                 "Model: " + carModel + "\n" +
                 "Fuel type: " + fuelType + "\n" +
                 "Emission: " + emissions + "\n" +
---
+++ src/main/java/ee/bcs/carportal/repository/CarRepositoryImpl.java
@@ -0,0 +1,55 @@
+package ee.bcs.carportal.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import ee.bcs.carportal.persistence.Car;
+import ee.bcs.carportal.persistence.FuelType;
+
+@Repository
+public class CarRepositoryImpl implements CarRepository {
+    private Map<Integer, Car> cars = createCars();
+
+    public void save(Car car) {
+        cars.put(car.getId(), car);
+    };
+
+    public Car getById(int id) {
+        if (cars.get(id) != null) {
+            return cars.get(id);
+        }
+        return null;
+    };
+
+    public List<Car> findAll() {
+        return new ArrayList<>(cars.values());
+    };
+
+    public void deleteById(int id) {
+        if (cars.get(id) != null) {
+            cars.remove(id);
+        }
+    };
+
+    // extra methods
+
+    // https://www.geeksforgeeks.org/hashmap-put-method-in-java/
+
+    private Map<Integer, Car> createCars() {
+        Map<Integer, Car> cars = new HashMap<>();
+        cars.put(1, new Car(1, "Model 3", "Tesla", 2020, FuelType.ELECTRIC, 0.0, 44000));
+        cars.put(2, new Car(2, "Civic", "Honda", 2021, FuelType.PETROL, 0.05, 25000));
+        cars.put(3, new Car(3, "Camry", "Toyota", 2022, FuelType.PETROL, 0.04, 28000));
+        cars.put(4, new Car(4, "F-150", "Ford", 2023, FuelType.PETROL, 0.1, 45000));
+        cars.put(5, new Car(5, "Prius", "Toyota", 2020, FuelType.HYBRID, 0.03, 30000));
+        return cars;
+    }
+
+    public void resetData() {
+        cars = createCars();
+    }
+}
