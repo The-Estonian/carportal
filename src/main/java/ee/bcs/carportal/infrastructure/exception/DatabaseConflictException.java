f58ed1252c6cced813b80668c895384c0877637a
QBIT PC <bsv.ttu@gmailcom>
2025-07-12 18:17:52 +0300

    CP-55 WIP

--- src/main/java/ee/bcs/carportal/infrastructure/exception/DatabaseConflictException.java
+++ src/main/java/ee/bcs/carportal/infrastructure/exception/DatabaseConflictException.java
@@ -1,4 +1,7 @@
 package ee.bcs.carportal.infrastructure.exception;
 
-public class DatabaseConflictException {
+public class DatabaseConflictException extends RuntimeException {
+    public DatabaseConflictException(String errorMessage) {
+        super(errorMessage);
+    }
 }
