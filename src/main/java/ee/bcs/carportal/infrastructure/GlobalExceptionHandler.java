b415b614e758c88b49396385fe49d1e6785acf72
QBIT PC <bsv.ttu@gmailcom>
2025-07-12 18:51:25 +0300

    CP-56 WIP

--- src/main/java/ee/bcs/carportal/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/ee/bcs/carportal/infrastructure/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import ee.bcs.carportal.infrastructure.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
